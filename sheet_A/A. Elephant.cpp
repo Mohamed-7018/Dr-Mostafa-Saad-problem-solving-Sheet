1662a3abfd7af62847d32a4860593cb2b62924d3
Mohamed-7018 <80456446+Mohamed-7018@users.noreply.github.com>
2022-08-27 21:53:15 +0200

    elephant

---
+++ sheet_A/A. Elephant.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define mk  make_pair
+#define vi  vector<int>
+#define vs  vector<string>
+#define vp  vector<pair<string, string>>
+#define vl  vector<long long>
+#define pqi priority_queue<int>
+#define qi  queue<int>
+#define sc  set<char>
+#define ss  set<string>
+#define si  set<int>
+#define pii pair<int, int>
+#define psv pair<string, vector<int>>
+#define mci map<char,int>
+#define msv map<string,vector<string>>
+
+
+int main() {
+    int n;
+    cin >>n;
+
+    for (int i=0; ;i++) {
+        if ((i*5) >=n) {
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
